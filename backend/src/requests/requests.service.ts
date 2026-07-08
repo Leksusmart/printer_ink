c9e42324f7176df2bdcd5141c8d577c7202fa3c4
Leksusmart <Leksusmartmail@gmail.com>
2026-07-08 15:56:55 +0300

    Добавлена функция генерации GUID

--- backend/src/requests/requests.service.ts
+++ backend/src/requests/requests.service.ts
@@ -77,4 +77,56 @@ export class RequestsService {
 		return result.rows[0];
 	}
 
+	async generateGUID(): Promise<{ guid: string }> {
+		let errCounter: number = 0;
+		const generationLimit: number = 100000;
+		const hexArr: string[] = '0123456789abcdef'.split('');
+		const variantArr: string[] = ['8', '9', 'a', 'b'];
+
+		const dbResult = await this.databaseService.query(`SELECT guid FROM cartridges`);
+		// Превращаем в Set для мгновенного поиска за O(1)
+		const existingGUIDs = new Set(
+			dbResult.rows.map((row: any) => row.guid?.toLowerCase())
+		);
+
+
+		let generatedGUID: string = "";
+		let isUnique: boolean = false;
+
+		do {
+			const guidChars: string[] = new Array(36);
+
+			for (let i = 0; i < 36; i++) {
+				if (i === 8 || i === 13 || i === 18 || i === 23) {
+					guidChars[i] = '-';
+				} else if (i === 14) {
+					guidChars[i] = '4'; // Версия 4
+				} else if (i === 19) {
+					// Вариант (8, 9, a или b)
+					guidChars[i] = variantArr[Math.floor(Math.random() * variantArr.length)];
+				} else {
+					// Любой случайный из hexArr
+					guidChars[i] = hexArr[Math.floor(Math.random() * hexArr.length)];
+				}
+			}
+
+			// Собираем массив в строку
+			generatedGUID = guidChars.join('');
+
+			// Проверяем уникальность
+			if (!existingGUIDs.has(generatedGUID)) {
+				isUnique = true;
+			}
+
+			errCounter++;
+		} while (!isUnique && errCounter < generationLimit);
+
+		if (!isUnique) {
+			throw new Error('Превышен лимит попыток генерации уникального GUID');
+		}
+
+		return { guid: generatedGUID };
+	}
+
+
 }
