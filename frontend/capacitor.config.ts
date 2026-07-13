235772fc6b1bfdfb5422e80c7b9f34cc69b6dd39
Leksusmart <leksusmartmail@gmail.com>
2026-07-14 00:21:10 +0300

    Delete frontend/capacitor.config.ts

--- frontend/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.myproject.app',
-  appName: 'Printer Ink',
-  webDir: 'out'
-};
-
-export default config;
