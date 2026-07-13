a81b2e02149fd3e5077d4b6101fd55ad5f0fb516
Leksusmart <Leksusmartmail@gmail.com>
2026-07-14 00:11:06 +0300

    Добавление возможности создания APK

---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.myproject.app',
+  appName: 'Printer Ink',
+  webDir: 'out'
+};
+
+export default config;
