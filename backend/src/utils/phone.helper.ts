71320cc7bc96c4ce9a3b0765628d5f5dcbe87c4e
Leksusmart <leksusmartmail@gmail.com>
2026-07-14 09:51:42 +0300

    Fix: added class-validator + class-transformer, created phone.helper.ts (main)

---
+++ backend/src/utils/phone.helper.ts
@@ -0,0 +1,9 @@
+export function normalizePhone(phone: string): string {
+  let checkedPhone = phone.trim();
+  if (checkedPhone.length === 11 && checkedPhone.startsWith('7')) {
+    checkedPhone = '+' + checkedPhone;
+  } else if (checkedPhone.length === 11 && checkedPhone.startsWith('8')) {
+    checkedPhone = '+7' + checkedPhone.slice(1);
+  }
+  return checkedPhone;
+}
